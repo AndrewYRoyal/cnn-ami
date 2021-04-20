2f37391d2ca387ae08e9ef5279bfafe995e3963a
unknown <aroyal641@gmail.com>
2021-04-19 20:21:48 -0700

    quick fix

--- predict.py
+++ predict.py
@@ -9,11 +9,11 @@ import argparse
 import pandas as pd
 import subprocess
 
-# parser = argparse.ArgumentParser('Predict meter end-use using AMI data.')
-# parser.add_argument('--cfg', dest='cfg', default='cfg.yaml', action='store')
-# args = parser.parse_args()
-# cfg = yaml.load(open(args.cfg))
-cfg = yaml.load(open('cfg.yaml')) #<< use interactively
+parser = argparse.ArgumentParser('Predict meter end-use using AMI data.')
+parser.add_argument('--cfg', dest='cfg', default='cfg.yaml', action='store')
+args = parser.parse_args()
+cfg = yaml.load(open(args.cfg))
+# cfg = yaml.load(open('cfg.yaml')) #<< use interactively
 
 startTime = time()
 print('Loading Data...')
